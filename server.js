1c3f1bd7fc6e05740d4f0f5c7ada25ba85278cc4
Oracle Public Cloud User <opc@instance-20240508-0106.sub12142206410.eisp.oraclevcn.com>
2024-06-12 08:23:53 +0000

    modify server

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 import express from "express";
-import https from "https";
-import fs from "fs";
+import https from "http";
 import { handler } from "./build/handler.js";
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 
-// SSL 인증서 파일 경로
-const options = {
-    key: fs.readFileSync(process.env.SSL_KEY_PATH),
-    cert: fs.readFileSync(process.env.SSL_CRT_PATH)
-};
-
 // 모든 요청을 SvelteKit 핸들러로 라우팅
 app.use(handler);
 
 // HTTP 서버 시작
-https.createServer(options, app).listen(3002, () => {
+https.createServer(app).listen(3002, () => {
     console.log('Server is running on https://localhost:3002');
 });
