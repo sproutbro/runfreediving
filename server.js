964aa3ed065c605eb7c1bc3b45ac4a19d9a593cd
bbcode <krsproutbro@gmail.com>
2024-06-12 16:52:43 +0900

    modify server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import http from "http";
+import https from "https";
 import fs from "fs";
 import { handler } from "./build/handler.js";
 import dotenv from 'dotenv';
@@ -8,15 +8,15 @@ dotenv.config();
 const app = express();
 
 // SSL 인증서 파일 경로
-// const options = {
-//     key: fs.readFileSync(process.env.SSL_KEY_PATH),
-//     cert: fs.readFileSync(process.env.SSL_CRT_PATH)
-// };
+const options = {
+    key: fs.readFileSync(process.env.SSL_KEY_PATH),
+    cert: fs.readFileSync(process.env.SSL_CRT_PATH)
+};
 
 // 모든 요청을 SvelteKit 핸들러로 라우팅
 app.use(handler);
 
 // HTTP 서버 시작
-http.createServer(app).listen(3002, () => {
+https.createServer(options, app).listen(3002, () => {
     console.log('Server is running on https://localhost:3002');
 });
